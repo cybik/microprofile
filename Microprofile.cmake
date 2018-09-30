e453bf39522f8bee8b3a2cab8a93d6b19de3be87
Renaud Lepage <root@cybikbase.com>
2018-09-30 13:17:17 -0700

    Some sanitization, some simplifications, overall readability.

---
+++ Microprofile.cmake
@@ -0,0 +1,11 @@
+set(SOURCES_MICROPROFILE
+
+    #${CMAKE_CURRENT_LIST_DIR}/
+    ${CMAKE_CURRENT_LIST_DIR}/microprofile.cpp
+)
+set(HEADERS_MICROPROFILE
+
+    #${CMAKE_CURRENT_LIST_DIR}/
+    ${CMAKE_CURRENT_LIST_DIR}/microprofile.h
+    ${CMAKE_CURRENT_LIST_DIR}/microprofile_html.h
+)
